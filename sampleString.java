ffcfe1c7f628874edb59cade9c1696d1a60735d7
atulgupta0688 <atulkumar_gupta@srmuniv.edu.in>
2017-07-09 11:38:46 +0530

    Add files via upload

---
+++ sampleString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be 'Main' only if the class is public. */
+class sampleString
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+char[] hiArray = { 'h', 'i', ' ', 'U', 's', 'e' , 'r' , '!' };
+String hiString = new String(hiArray);
+System.out.println(hiString);
+}
+}
